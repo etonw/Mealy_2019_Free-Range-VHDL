810ea3795e0173099de8e3f8f46a994187f46bf4
etonw <eton.williams@gmail.com>
2018-09-05 15:41:48 +0200

    hieu introduction to using a comparator in VHDL

---
+++ Hieu_two_bit_comparator/Hieu_two_bit_comparator.srcs/sources_1/new/one_bit_comparator.v
@@ -0,0 +1,28 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 04.09.2018 13:07:40
+// Design Name: 
+// Module Name: one_bit_comparator
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module one_bit_comparator(
+    input a,
+    input b,
+    output z
+    );
+endmodule
